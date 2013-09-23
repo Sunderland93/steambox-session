4786cd122bc219b78305931ee3c81ce19480c01d
Fabio Erculiani <lxnay@sabayon.org>
2013-09-23 10:14:48 +0200

    Fix sabayon-steambox.sh helper, properly use LIVE_USER

--- libexec/sabayon-steambox.sh
+++ libexec/sabayon-steambox.sh
@@ -5,12 +5,12 @@ SABAYON_USER=$(cat /etc/sabayon/steambox-user 2>/dev/null)
 
 if sabayon_is_steambox; then
 
-    _HOME="/home/${SABAYON_USER}"
-    echo "Sabayon Steam Box mode enabled"
+    _HOME="/home/${LIVE_USER}"
+    echo "Sabayon Steam Box mode enabled, user: ${LIVE_USER}"
 
     echo "[Desktop]" > "${_HOME}"/.dmrc
-    echo "Session=${SABAYON_USER}" >> "${_HOME}"/.dmrc
-    chown "${SABAYON_USER}" "${_HOME}"/.dmrc
+    echo "Session=steambox" >> "${_HOME}"/.dmrc
+    chown "${LIVE_USER}" "${_HOME}"/.dmrc
     if [ -x "/usr/libexec/gdm-set-default-session" ]; then
         # TODO: remove this in 6 months
         # oh my fucking glorious god, this
@@ -20,7 +20,7 @@ if sabayon_is_steambox; then
     fi
     if [ -x "/usr/libexec/gdm-set-session" ]; then
         # GDM 3.6 support
-        /usr/libexec/gdm-set-session "${SABAYON_USER}" steam
+        /usr/libexec/gdm-set-session "${LIVE_USER}" steam
     fi
     sabayon_setup_autologin
 
