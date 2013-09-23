cc19e7e179d30afcfb7ae5b1bff0c4814c4b5a41
Fabio Erculiani <lxnay@sabayon.org>
2013-09-23 17:31:29 +0200

    Another set of improvements and code reorganization

--- manager/manager.py
+++ manager/manager.py
@@ -6,6 +6,8 @@ import subprocess
 import threading
 import time
 
+import cairo
+
 from gi.repository import Gtk, Gdk, GLib
 
 class Manager(object):
@@ -17,7 +19,7 @@ class Manager(object):
             os.path.join(os.getcwd(), "manager.ui"))
 
         self._window = self._builder.get_object("mainWindow")
-        self._window.connect("delete-event", self._on_delete)
+        self._win_setup()
 
         self._steam = self._builder.get_object("steamButton")
         self._steam.connect("clicked", self._on_steam)
@@ -45,11 +47,35 @@ class Manager(object):
         time.sleep(5)
         os.killpg(os.getpgid(0), signal.SIGKILL)
 
+    def _win_setup(self):
+        self._window.connect("delete-event", self._on_delete)
+        self._window.connect("draw", self._on_win_draw)
+
+        self._window.set_app_paintable(True)
+        screen = self._window.get_screen()
+        if screen is not None:
+            visual = screen.get_rgba_visual()
+            if visual is not None and screen.is_composited():
+                self._window.set_visual(visual)
+
+        self._window.show()
+        self._window.set_keep_below(True)
+
+        cursor = Gdk.Cursor(Gdk.CursorType.TARGET)
+        gdk_w = self._window.get_root_window()
+        gdk_w.set_cursor(cursor)
+
+        self._on_steam(None)
+
+    def _on_win_draw(self, wid, cr):
+        cr.set_source_rgba(0.2, 0.2, 0.2, 1.0)
+        cr.set_operator(cairo.OPERATOR_SOURCE)
+        cr.paint()
+
     def run(self):
         """
         Run Steam Manager ;-)
         """
-        GLib.idle_add(self._window.show)
         GLib.threads_init()
         Gdk.threads_enter()
         Gtk.main()
