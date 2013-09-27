a2b8ccf4ade41b906d206e0bace07b738c83a5bc
Fabio Erculiani <lxnay@sabayon.org>
2013-09-27 10:03:31 +0200

    sabayon-steambox.sh: revert gdm session to gnome if steambox is disabled

--- libexec/sabayon-steambox.sh
+++ libexec/sabayon-steambox.sh
@@ -3,6 +3,24 @@
 SABAYON_USER=$(cat /etc/sabayon/steambox-user 2>/dev/null)
 . /sbin/sabayon-functions.sh
 
+_set_gdm_session() {
+    if [ ! -x /usr/bin/gdm ]; then
+        return 0
+    fi
+
+    if [ -x "/usr/libexec/gdm-set-default-session" ]; then
+        # TODO: remove this in 6 months
+        # oh my fucking glorious god, this
+        # is AccountsService bullshit
+        # cross fingers
+        /usr/libexec/gdm-set-default-session "${1}"
+    fi
+    if [ -x "/usr/libexec/gdm-set-session" ]; then
+        # GDM 3.6 support
+        /usr/libexec/gdm-set-session "${LIVE_USER}" "${1}"
+    fi
+}
+
 if sabayon_is_steambox; then
 
     _HOME="/home/${LIVE_USER}"
@@ -11,17 +29,7 @@ if sabayon_is_steambox; then
     echo "[Desktop]" > "${_HOME}"/.dmrc
     echo "Session=steambox" >> "${_HOME}"/.dmrc
     chown "${LIVE_USER}" "${_HOME}"/.dmrc
-    if [ -x "/usr/libexec/gdm-set-default-session" ]; then
-        # TODO: remove this in 6 months
-        # oh my fucking glorious god, this
-        # is AccountsService bullshit
-        # cross fingers
-        /usr/libexec/gdm-set-default-session steambox
-    fi
-    if [ -x "/usr/libexec/gdm-set-session" ]; then
-        # GDM 3.6 support
-        /usr/libexec/gdm-set-session "${LIVE_USER}" steambox
-    fi
+    _set_gdm_session steambox
     sabayon_setup_autologin
 
     # dive into the final executable
@@ -32,6 +40,7 @@ fi
 if ! sabayon_is_live && ! sabayon_is_steambox; then
     echo "Sabayon Steam Box mode disabled"
     sabayon_disable_autologin
+    _set_gdm_session gnome
 fi
 
 while true; do
